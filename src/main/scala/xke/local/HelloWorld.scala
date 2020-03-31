196423b0857b6497b193bfa2ccee5a577cb4a345
Mickael Moreira <dopidot@gmail.com>
2020-03-31 15:02:50 +0200

    Exercice departement

--- src/main/scala/xke/local/HelloWorld.scala
+++ src/main/scala/xke/local/HelloWorld.scala
@@ -15,8 +15,27 @@ object HelloWorld {
     // 3) renommer la colonne moyenne des départements en avg_dep
     // 4) écrire le fichier en parquet
 
+    val df = spark.read.option("delimiter", ",").option("header", true).csv("src/main/resources/departements-france.csv")
+
+    val test = df.withColumn("code_departement", col("code_departement").cast("integer"))
+
+    /*val test = df.withColumn("code_departement", col("code_departement").cast("integer"))
+      .groupBy("code_region")
+      .avg("code_departement").as("avg_dep")
+      .show()*/
+    val test2 = avgDepByReg(test)
+    val test3 = renameColumn(test2).show()
   }
 
-  def avgDepByReg: DataFrame = ???
-  def renameColumn: DataFrame = ???
+
+  //def avgDepByReg: DataFrame = ???
+  //def renameColumn: DataFrame = ???
+
+  def avgDepByReg(dataFrame: DataFrame) : DataFrame = {
+    dataFrame.groupBy("code_region").avg("code_departement")
+  }
+
+  def renameColumn(dataFrame: DataFrame) : DataFrame = {
+    dataFrame.withColumnRenamed("avg(code_departement)", "avg_dep")
+  }
 }
