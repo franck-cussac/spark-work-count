b1ddb17cbe790a4db4a125b2ef9c603537781566
Pinaworld <pinben02@gmail.com>
2020-03-31 16:06:17 +0200

    ShouldBeRightWithoutTest

--- src/main/scala/xke/local/HelloWorld.scala
+++ src/main/scala/xke/local/HelloWorld.scala
@@ -1,6 +1,5 @@
 package xke.local
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.util.IntType
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
@@ -9,13 +8,15 @@ import org.apache.spark.sql.types.IntegerType
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local[*]").appName("").getOrCreate()
-
     val df = spark.read.option("delimiter", ",").option("header", true).csv("src/main/resources/departements-france.csv")
 
     val dfIntColumn = df.withColumn("code_departement", col("code_departement").cast(IntegerType))
     val newDF = avgDepByReg(dataFrame = dfIntColumn)
-    val newDfName = renameColumn(dataFrame = newDF)
-    newDfName.show
+    val newDfColumn = newColumn(dataFrame = newDF)
+    val newDfName = renameColumn(dataFrame = newDfColumn)
+
+    newDfName.write.mode(SaveMode.Overwrite)parquet("ParquetResult")
+    spark.read.parquet("ParquetResult").show
   }
 
   def avgDepByReg(dataFrame: DataFrame): DataFrame = {
@@ -23,6 +24,10 @@ object HelloWorld {
   }
 
   def renameColumn(dataFrame: DataFrame): DataFrame = {
-    dataFrame.withColumnRenamed("average","avg_dep")
+    dataFrame.withColumnRenamed("avg(code_departement)","avg_dep")
+  }
+
+  def newColumn(dataFrame: DataFrame): DataFrame = {
+    dataFrame.withColumn("average", col("avg(code_departement)"))
   }
 }
