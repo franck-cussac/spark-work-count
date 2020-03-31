af8e488b56fcb6eb76bf4db86856cab0422488b9
MBouzeboudja <mahdibouzeboudja@yahoo.fr>
2020-03-31 16:05:54 +0200

    Exo1 v1

--- src/main/scala/xke/local/HelloWorld.scala
+++ src/main/scala/xke/local/HelloWorld.scala
@@ -1,11 +1,13 @@
 package xke.local
 
+import io.netty.handler.codec.spdy.DefaultSpdyDataFrame
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.IntegerType
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("test").master("local[*]").getOrCreate()
+    val spark = SparkSession.builder().appName("Mahdi").master("local[*]").getOrCreate()
 
     // code
     // src/main/resources/departements-france.csv
@@ -14,9 +16,29 @@ object HelloWorld {
     //    code_region, avg_dep, nom_region
     // 3) renommer la colonne moyenne des départements en avg_dep
     // 4) écrire le fichier en parquet
+    if(args.length >= 2) {
+      val result = readDataAndApplyTransformations(spark, args(0))
+      result.write.mode(SaveMode.Overwrite).parquet(args(1))
+      result.show()
+    }else {
+      val result = readDataAndApplyTransformations(spark, "src/main/resources/departements-france.csv")
+      result.write.mode(SaveMode.Overwrite).parquet("result")
+      result.show()
+    }
 
   }
 
-  def avgDepByReg: DataFrame = ???
-  def renameColumn: DataFrame = ???
+  private def readDataAndApplyTransformations(spark: SparkSession, dataPath: String): DataFrame = {
+    val df = spark.read.option("delimiter", ",").option("header", value = true).option("inferSchema", "true").csv(dataPath)
+    val intermediate = avgDepByReg(df)
+    val result = renameColumn(intermediate, "avg(code_departement)", "avg_dep")
+    result;
+  }
+
+  def avgDepByReg: DataFrame => DataFrame =
+    (df: DataFrame) =>  df.groupBy("code_region", "nom_region").agg(avg("code_departement"))
+
+  def renameColumn(dataFrame: DataFrame, oldName: String, newName:String): DataFrame = {
+    dataFrame.withColumnRenamed(oldName, newName)
+  }
 }
--- src/test/scala/xke/local/HelloWorldTest.scala
+++ src/test/scala/xke/local/HelloWorldTest.scala
@@ -1,5 +1,8 @@
 package xke.local
 
+import javax.annotation.meta
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.IntegerType
 import org.scalatest.{FunSuite, GivenWhenThen}
 import spark.{DataFrameAssertions, SharedSparkSession}
 
@@ -35,24 +38,71 @@ class HelloWorldTest extends FunSuite with GivenWhenThen with DataFrameAssertion
   }*/
 
   test("je veux ajouter une colonne avec la moyenne des numéros département par région") {
+    Given("Une data frame")
+    val input = spark.sparkContext.parallelize(
+      List(
+        (1, 64, "Auvergne-Rhône-Alpes"),
+        (1, 24, "Auvergne-Rhône-Alpes"),
+        (1, 95, "Auvergne-Rhône-Alpes"),
+        (2, 75, "iles-de-france"),
+        (2, 56, "iles-de-france"),
+        (2, 46, "iles-de-france")
 
+      )).toDF( "code_region", "code_departement", "nom_region")
+
+    val expected = spark.sparkContext.parallelize(
+      List(
+        (1, 61, "Auvergne-Rhône-Alpes"),
+        (2, 59, "iles-de-france")
+      )).toDF("code_region", "avg(code_departement)", "nom_region")
+
+    When("J'applique ma fonction avgDepByReg je dois avoir une colonne avec la moyenne des numéros département par région")
+    val actual =   HelloWorld.avgDepByReg(input)
+    assertDataFrameEquals(actual, expected);
   }
 
   test("je veux renommer la colonne des moyennes des numéros département") {
 
     Given("une dataframe avec au moins 3 colonnes : nom région, code région et numéro département")
-    val input = ???
-    val expected = ???
+    val input = spark.sparkContext.parallelize(
+      List(
+        (1, 61, "Auvergne-Rhône-Alpes"),
+        (2, 59, "iles-de-france")
+      )).toDF("code_region", "avg(code_departement)", "nom_region")
 
-    When("")
-    val actual = HelloWorld.avgDepByReg(input)
+    val expected = spark.sparkContext.parallelize(
+      List(
+        (1, 61, "Auvergne-Rhône-Alpes"),
+        (2, 59, "iles-de-france")
+      )).toDF("code_region", "avg_dep", "nom_region")
 
-    Then("")
+    When("On renme la colonne 'avg(code_departement)' avec 'avg_dep'")
+    val actual = HelloWorld.renameColumn(input, "avg(code_departement)", "avg_dep")
+
+    Then("La colonne 'avg(code_departement)' devra être renommée avec 'avg_dep'")
+
     assertDataFrameEquals(actual, expected)
+    val columnsSet =    actual.columns.toSet
+    columnsSet shouldEqual  Set("code_region", "avg_dep", "nom_region")
+    columnsSet should not contain "avg(code_departement)"
   }
 
   test("je veux vérifier que je lis un fichier, ajoute une colonne, la renomme, et sauvegarde mon fichier en parquet") {
+    Given("Un chemin vers un jeu de de données et un chemin où stocker le résultat")
+    val spark =  SparkSession.builder().master("local[*]").appName("test").getOrCreate()
+    val pathResult = "result"
+    val pathData   = "src/main/resources/departements-france.csv"
+    val args = Array(pathData, pathResult)
 
+    When("On excute le main")
+    HelloWorld.main(args)
+    val df = spark.read.parquet(pathResult)
+
+    Then("Il faut qu'on trouve la nouvelle colonne 'avg_dep' et ne pas trouver la colonne 'avg(code_departement)'")
+    val columnsSet =    df.columns.toSet
+    columnsSet shouldEqual  Set("code_region", "avg_dep", "nom_region")
+    columnsSet should not contain "avg(code_departement)"
+    df.select("avg_dep").count() should not equal(0)
   }
 
 }
