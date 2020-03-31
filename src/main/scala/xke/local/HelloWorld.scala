d0edba11aa30a5fd5c3d5b0ce488475907a0a137
ItIsMagic <morabi.andrea@gmail.com>
2020-03-31 14:31:33 +0200

    avancement

--- src/main/scala/xke/local/HelloWorld.scala
+++ src/main/scala/xke/local/HelloWorld.scala
@@ -15,8 +15,22 @@ object HelloWorld {
     // 3) renommer la colonne moyenne des départements en avg_dep
     // 4) écrire le fichier en parquet
 
+    val df = spark.read.option("sep", ",").option("header", true).csv("C:\\hadoop\\project\\spark-work-count\\src\\main\\resources\\departements-france.csv")
+    val avg = avgDepByReg(df)
+    avg.show
+
+    val renamed = renameColumn(avg, "avg_dep", "avg_departement")
+    renamed.show
   }
 
-  def avgDepByReg: DataFrame = ???
-  def renameColumn: DataFrame = ???
+  def avgDepByReg(dataFrame: DataFrame): DataFrame = {
+    dataFrame.withColumn("code_departement", dataFrame.col("code_departement")
+      .cast("Double"))
+      .groupBy(col("code_region"))
+      .agg(avg("code_departement").as("avg_departement"),max("nom_region").as("nom_region"))
+  }
+
+  def renameColumn(dataFrame: DataFrame, newName: String, oldName: String): DataFrame = {
+    dataFrame.withColumnRenamed(oldName, newName)
+  }
 }
