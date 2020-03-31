8736d2329ad09d3bf343c9543c2349bf02913d27
ElCammel <lamygregoire1@gmail.com>
2020-03-31 16:16:33 +0200

    "cleaning code"

--- src/main/scala/xke/local/HelloWorld.scala
+++ src/main/scala/xke/local/HelloWorld.scala
@@ -1,6 +1,7 @@
 package xke.local
 
 import com.sun.org.apache.xalan.internal.xsltc.compiler.util.IntType
+import org.apache.avro.generic.GenericData.StringType
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
@@ -10,29 +11,13 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local[*]").appName("").getOrCreate()
 
-    /*import spark.implicits._
-
-    val inputFile = args(0)
-    val outputFile = args(1)
-    val input =  spark.sparkContext.textFile(inputFile)
-    val words = input.flatMap(line => line.split(" "))
-    val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
-    counts.toDF("word", "count").write.mode(SaveMode.Overwrite).parquet(outputFile)*/
-
-    // 1) lire le fichier
-    // 2) créer une colonne avec la moyenne des numéro département par code région
-    //    code_region, avg_dep, nom_region
-    // 3) renommer la colonne moyenne des départements en avg_dep
-    // 4) écrire le fichier en parquet
-
-    val df = spark.read.option("delimiter", ",").option("header", true).csv("src/main/resources/departements-france.csv")
-
+    val df = readCSVFile(spark, "src/main/resources/departements-france.csv")
     val dfIntColumn = df.withColumn("code_departement", col("code_departement").cast(IntegerType))
     val newDF = avgDepByReg(dataFrame = dfIntColumn)
-    val newDfColumn = newColumn(dataFrame = newDF)
-    val newDfName = renameColumn(dataFrame = newDfColumn)
+    val newDfColumn = newColumn(dataFrame = newDF, columnName = "average", value = col("avg(code_departement)"))
+    val newDfName = renameColumn(dataFrame = newDfColumn, columnName = "avg(code_departement)", newName = "avg_dep")
 
-    writeParquet(dataFrame = newDfName)
+    writeParquet(dataFrame = newDfName, "ParquetResult")
 
     spark.read.parquet("ParquetResult").show
   }
@@ -41,15 +26,19 @@ object HelloWorld {
     dataFrame.groupBy(col("code_region")).avg("code_departement").as("avg_dep")
   }
 
-  def renameColumn(dataFrame: DataFrame): DataFrame = {
-    dataFrame.withColumnRenamed("avg(code_departement)","avg_dep")
+  def renameColumn(dataFrame: DataFrame, columnName: String, newName: String): DataFrame = {
+    dataFrame.withColumnRenamed(columnName,newName)
   }
 
-  def newColumn(dataFrame: DataFrame): DataFrame = {
-    dataFrame.withColumn("average", col("avg(code_departement)"))
+  def newColumn(dataFrame: DataFrame, columnName: String, value: Column): DataFrame = {
+    dataFrame.withColumn(columnName, value)
   }
 
-  def writeParquet(dataFrame: DataFrame){
-    dataFrame.write.mode(SaveMode.Overwrite).parquet("ParquetResult")
+  def writeParquet(dataFrame: DataFrame, outputName: String){
+    dataFrame.write.mode(SaveMode.Overwrite).parquet(outputName)
+  }
+
+  def readCSVFile(spark: SparkSession, pathToFile: String): DataFrame = {
+    spark.read.option("delimiter", ",").option("header", true).csv(pathToFile)
   }
 }
