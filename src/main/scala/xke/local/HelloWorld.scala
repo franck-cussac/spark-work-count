d40eec027a68e7b1f73b30ebcf4f698d04025bf5
ElCammel <lamygregoire1@gmail.com>
2020-03-31 14:49:13 +0200

    exo 1

--- src/main/scala/xke/local/HelloWorld.scala
+++ src/main/scala/xke/local/HelloWorld.scala
@@ -1,17 +1,39 @@
 package xke.local
 
 import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().getOrCreate()
-    import spark.implicits._
+    val spark = SparkSession.builder().master("local[*]").appName("").getOrCreate()
+
+    /*import spark.implicits._
 
     val inputFile = args(0)
     val outputFile = args(1)
     val input =  spark.sparkContext.textFile(inputFile)
     val words = input.flatMap(line => line.split(" "))
     val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
-    counts.toDF("word", "count").write.mode(SaveMode.Overwrite).parquet(outputFile)
+    counts.toDF("word", "count").write.mode(SaveMode.Overwrite).parquet(outputFile)*/
+    // 1) lire le fichier
+    // 2) créer une colonne avec la moyenne des numéro département par code région
+    //    code_region, avg_dep, nom_region
+    // 3) renommer la colonne moyenne des départements en avg_dep
+    // 4) écrire le fichier en parquet
+
+    val df = spark.read.option("delimiter", ",").option("header", true).csv("src/main/resources/departements-france.csv")
+
+    val newDF = avgDepByReg(dataFrame = df)
+    val newDfName = renameColumn(dataFrame = newDF)
+    newDfName.show
+  }
+
+  def avgDepByReg(dataFrame: DataFrame): DataFrame = {
+    dataFrame.groupBy(col("code_region")).avg("code_departement").as("avg_dep")
+  }
+
+  def renameColumn(dataFrame: DataFrame): DataFrame = {
+    dataFrame.withColumnRenamed("average","avg_dep")
   }
 }
--- src/test/scala/xke/local/HelloWorldTest.scala
+++ src/test/scala/xke/local/HelloWorldTest.scala
@@ -7,30 +7,41 @@ class HelloWorldTest extends FunSuite with GivenWhenThen with DataFrameAssertion
   val spark = SharedSparkSession.sparkSession
   import spark.implicits._
 
-  test("main must create a file with word count result") {
-    Given("input filepath and output filepath")
-    val input = "src/test/resources/input.txt"
-    val output = "src/test/resources/output/v1/parquet"
+  //  test("main must create a file with word count result") {
+  //    Given("input filepath and output filepath")
+  //    val input = "src/test/resources/input.txt"
+  //    val output = "src/test/resources/output/v1/parquet"
+  //
+  //    When("I call word count")
+  //    HelloWorld.main(Array(input, output))
+  //    val expected = spark.sparkContext.parallelize(
+  //      List(("rapidement",1),
+  //        ("te",1),
+  //        ("à",1),
+  //        ("mots",1),
+  //        ("des",1),
+  //        ("s'il",1),
+  //        ("compter",1),
+  //        ("Bonjour,",1),
+  //        ("as",1),
+  //        ("plait.",1),
+  //        ("tu",1))
+  //    ).toDF("word", "count")
+  //
+  //    Then("I can read output file and find my values")
+  //    val actually = spark.sqlContext.read.parquet(output)
+  //
+  //    assertDataFrameEquals(actually, expected)
+  //  }
+  test("Je veux ajouter une colonne avec la moyen,ne des numéros département par région") {
 
-    When("I call word count")
-    HelloWorld.main(Array(input, output))
-    val expected = spark.sparkContext.parallelize(
-      List(("rapidement",1),
-        ("te",1),
-        ("à",1),
-        ("mots",1),
-        ("des",1),
-        ("s'il",1),
-        ("compter",1),
-        ("Bonjour,",1),
-        ("as",1),
-        ("plait.",1),
-        ("tu",1))
-    ).toDF("word", "count")
+  }
 
-    Then("I can read output file and find my values")
-    val actually = spark.sqlContext.read.parquet(output)
+  test("Je veux renommer la colonne des moyennes des numéros département") {
 
-    assertDataFrameEquals(actually, expected)
+  }
+
+  test("Je veux vérifier que je lis un fichier, ajoute une colonne, la renomme, et suavegarde mon fichier en parquet") {
+
   }
 }
