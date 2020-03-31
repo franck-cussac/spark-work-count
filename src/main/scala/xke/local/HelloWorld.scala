1a15e1d9424e958434d03ad1cfec7ad9a85ef018
ElCammel <lamygregoire1@gmail.com>
2020-03-31 16:07:35 +0200

    Added Parquet Write function

--- src/main/scala/xke/local/HelloWorld.scala
+++ src/main/scala/xke/local/HelloWorld.scala
@@ -1,8 +1,10 @@
 package xke.local
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.IntType
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.IntegerType
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
@@ -16,6 +18,7 @@ object HelloWorld {
     val words = input.flatMap(line => line.split(" "))
     val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
     counts.toDF("word", "count").write.mode(SaveMode.Overwrite).parquet(outputFile)*/
+
     // 1) lire le fichier
     // 2) créer une colonne avec la moyenne des numéro département par code région
     //    code_region, avg_dep, nom_region
@@ -24,9 +27,14 @@ object HelloWorld {
 
     val df = spark.read.option("delimiter", ",").option("header", true).csv("src/main/resources/departements-france.csv")
 
-    val newDF = avgDepByReg(dataFrame = df)
-    val newDfName = renameColumn(dataFrame = newDF)
-    newDfName.show
+    val dfIntColumn = df.withColumn("code_departement", col("code_departement").cast(IntegerType))
+    val newDF = avgDepByReg(dataFrame = dfIntColumn)
+    val newDfColumn = newColumn(dataFrame = newDF)
+    val newDfName = renameColumn(dataFrame = newDfColumn)
+
+    writeParquet(dataFrame = newDfName)
+
+    spark.read.parquet("ParquetResult").show
   }
 
   def avgDepByReg(dataFrame: DataFrame): DataFrame = {
@@ -34,6 +42,14 @@ object HelloWorld {
   }
 
   def renameColumn(dataFrame: DataFrame): DataFrame = {
-    dataFrame.withColumnRenamed("average","avg_dep")
+    dataFrame.withColumnRenamed("avg(code_departement)","avg_dep")
+  }
+
+  def newColumn(dataFrame: DataFrame): DataFrame = {
+    dataFrame.withColumn("average", col("avg(code_departement)"))
+  }
+
+  def writeParquet(dataFrame: DataFrame){
+    dataFrame.write.mode(SaveMode.Overwrite).parquet("ParquetResult")
   }
 }
