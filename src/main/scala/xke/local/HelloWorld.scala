08890a55244ea01a2a43d9ded44ae0fe37013978
MIVI <mickael.vilalard@hotmail.fr>
2020-04-01 12:03:39 +0200

    Tests

--- src/main/scala/xke/local/HelloWorld.scala
+++ src/main/scala/xke/local/HelloWorld.scala
@@ -5,35 +5,30 @@ import org.apache.spark.sql.functions._
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("MIVI").master("local[*]").getOrCreate()
+    val spark = SparkSession.builder().appName("test").master("local[*]").getOrCreate()
 
-    // code
-    // src/main/resources/departements-france.csv
-    // 1) lire le fichier
-    // 2) créer une colonne avec la moyenne des numéro département par code région
-    //    code_region, avg_dep, nom_region
-    // 3) renommer la colonne moyenne des départements en avg_dep
-    // 4) écrire le fichier en parquet
-
-    val df = spark.read.option("header", "true")
-      .option("delimiter", ",")
-      .csv("src/main/resources/departements-france.csv")
-//      .withColumn("code_departement", col("code_departement").cast("integer"))
-//      .groupBy("code_region")
-//      .avg("code_departement").as("avg_dep")
-//      .show()
-
-    val df2 = avgDepByReg(dataFrame = df)
-    val df3 = renameColumn(dataFrame = df2)
-    df3.show()
-
-  }
-  def avgDepByReg(dataFrame: DataFrame): DataFrame = {
-    dataFrame.groupBy("code_region")
-      .avg("code_departement").as("avg_dep")
+    val df = spark.read.option("sep", ",")
+      .option("header", true)
+      .csv("src/main/resources/departements-france-short.csv")
+    val avg = avgDepByReg(df)
+    val rename = renameColumn(avg)
+    rename.show()
+    writeToParquet(rename)
   }
 
-  def renameColumn(dataFrame: DataFrame): DataFrame = {
-    dataFrame.withColumnRenamed("average","avg_dep")
+  def avgDepByReg(input: DataFrame): DataFrame = {
+    input
+      .groupBy("code_region", "nom_region")
+      .agg(avg("code_departement")/*, first("nom_region").as("nom_region")*/)
+  }
+
+  def renameColumn(input: DataFrame): DataFrame = {
+    input
+      .withColumnRenamed("avg(code_departement)", "avg_dep")
+  }
+
+  def writeToParquet(input: DataFrame)  = {
+    input.write.mode("overwrite")
+      .parquet("src/main/parquet/ex1.parquet")
   }
 }
