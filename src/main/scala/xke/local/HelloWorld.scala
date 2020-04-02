780621602768c384e008a15b5b83ec9a78f3a395
Alexis Delée <alexisdelee@protonmail.com>
2020-04-02 16:13:26 +0200

    02/04/2020 16:13

--- src/main/scala/xke/local/HelloWorld.scala
+++ src/main/scala/xke/local/HelloWorld.scala
@@ -11,7 +11,7 @@ object HelloWorld {
     // code
     // src/main/resources/departements-france.csv
     // 1) lire le fichier
-    val df = spark.read
+    val df1 = spark.read
       .option("delimiter", ",")
       .option("header", true)
       .option("inferSchema", true)
@@ -24,26 +24,127 @@ object HelloWorld {
       .filter(col("count") > 5)
 
     // 5) afficher le nombre de lignes finales
-    df.show()
+    df1.show()
 
     // code
     // src/main/resources/departements-france.csv
     // 1) lire le fichier
+    val df2 = spark.read
+      .option("delimiter", ",")
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("src/main/resources/departements-france.csv")
     // 2) créer une colonne avec la moyenne des numéros de département par code région
+    val df2_avgDepByReg = HelloWorld.avgDepByReg(df2)
       // code_region, avg_dep, nom_region
     // 3) renommer la colonne moyenne des numéros de département en avg_dep
-    // 4) écrire le fichier en parquet
+    val df2_renameColumn = HelloWorld.renameColumns(
+      df2_avgDepByReg,
+      Map("avg(code_departement)" -> "avg_dep")
+    )
+    // 4) afficher le nombre de lignes finales
+    df2_renameColumn.show()
+
+    // code
+    // créer une UDF
+    // 1) qui prend un String en paramètre et renvoie un Int
+    // 2) si votre String commence par un 0, on l'enlève
+    // 3) si votre String contient un caractère non numérique, on l'enlève
+    // 4) faire un test unitaire également
+
+    // code
+    // jointure au niveau des numéros de département entre le fichier regions.csv + departments.csv et cities.csv + departement.csv
+    val df3_departements = spark.read
+      .option("delimiter", ",")
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("src/main/resources/departments.csv");
+
+    val df3_departments_renamed = HelloWorld.renameColumns(
+      df3_departements,
+      Map(
+        "id" -> "department_id",
+        "region_code" -> "department_region_code",
+        "code" -> "department_code",
+        "name" -> "department_name",
+        "slug" -> "department_slug"
+      )
+    )
+
+    val df3_regions = spark.read
+      .option("delimiter", ",")
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("src/main/resources/regions.csv")
+
+    val df3_regions_renamed = HelloWorld.renameColumns(
+      df3_regions,
+      Map(
+        "id" -> "region_id",
+        "code" -> "region_code",
+        "name" -> "region_name",
+        "slug" -> "region_slug"
+      )
+    )
+
+    val df3_cities = spark.read
+      .option("delimiter", ",")
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("src/main/resources/cities.csv")
+
+    val df3_cities_renamed = HelloWorld.renameColumns(
+      df3_cities,
+      Map(
+        "id" -> "city_id",
+        "department_code" -> "city_department_code",
+        "insee_code" -> "city_insee_code",
+        "zip_code" -> "city_zip_code",
+        "name" -> "city_name",
+        "slug" -> "city_slug",
+        "gps_lat" -> "city_gps_lat",
+        "gps_lng" -> "city_gps_lng"
+      )
+    )
+
+    val df3_departments_union_regions = df3_departments_renamed.join(
+      df3_regions_renamed,
+      df3_departments_renamed("department_region_code") === df3_regions_renamed("region_code"),
+      "left"
+    )
+
+    val df3_departements_union_regions_union_cities = df3_cities_renamed.join(
+      df3_departments_union_regions,
+      df3_cities_renamed("city_department_code") === df3_departments_union_regions("department_code"),
+      "left"
+    )
+
+    df3_departements_union_regions_union_cities.show()
+    df3_departements_union_regions_union_cities.write.mode(SaveMode.Overwrite).parquet("src/resources/output/v1/departments_regions_cities.parquet")
+  }
+
+  def extractCode(code: String): Int = {
+    code match {
+      case c if c.head == '0' => c.substring(1).toInt
+      case c if "AB" contains code.last => c.dropRight(1).toInt
+      case c => c.toInt
+    }
   }
 
   def avgDepByReg(df: DataFrame): DataFrame = {
+    val extractCodeUdf = udf(extractCode _)
+
     df
-      .withColumn("code_departement", col("code_departement").cast("integer"))
-      // .filter(row => row.getAs[String]("code_departement").matches("""\d+"""))
+      .withColumn("code_departement", extractCodeUdf(col("code_departement")))
       .groupBy("code_region", "nom_region")
       .avg("code_departement")
   }
 
-  def renameColumn(df: DataFrame, selectedColumn: String, renamedColumn: String): DataFrame = {
-    df.withColumnRenamed(selectedColumn, renamedColumn)
+  /* def renameColumn(df: DataFrame, selectedColumn: String, newColumn: String): DataFrame = {
+    HelloWorld.renameColumns(df, Map(selectedColumn -> newColumn))
+  } */
+
+  def renameColumns(df: DataFrame, selectedColumns: Map[String, String]): DataFrame = {
+    selectedColumns.foldLeft(df)((acc, item) => acc.withColumnRenamed(item._1, item._2))
   }
 }
--- src/test/scala/xke/local/HelloWorldTest.scala
+++ src/test/scala/xke/local/HelloWorldTest.scala
@@ -1,5 +1,6 @@
 package xke.local
 
+import org.apache.spark.sql.SaveMode
 import org.scalatest.{FunSuite, GivenWhenThen}
 import spark.{DataFrameAssertions, SharedSparkSession}
 
@@ -14,18 +15,18 @@ class HelloWorldTest extends FunSuite with GivenWhenThen with DataFrameAssertion
 
     When("I call word count")
     HelloWorld.main(Array(input, output))
-    val expected = spark.sparkContext.parallelize(
-      List(("rapidement",1),
-        ("te",1),
-        ("à",1),
-        ("mots",1),
-        ("des",1),
-        ("s'il",1),
-        ("compter",1),
-        ("Bonjour,",1),
-        ("as",1),
-        ("plait.",1),
-        ("tu",1))
+    val expected = List(
+      ("rapidement",1),
+      ("te",1),
+      ("à",1),
+      ("mots",1),
+      ("des",1),
+      ("s'il",1),
+      ("compter",1),
+      ("Bonjour,",1),
+      ("as",1),
+      ("plait.",1),
+      ("tu",1)
     ).toDF("word", "count")
 
     Then("I can read output file and find my values")
@@ -34,44 +35,60 @@ class HelloWorldTest extends FunSuite with GivenWhenThen with DataFrameAssertion
     assertDataFrameEquals(actually, expected)
   }
 
+  test("Je veux pouvoir récupérer l'entier dans un code département") {
+    Given("Une liste d'une colonne : code_departement")
+    val input = List("110", "011", "2A", "2B")
+
+    When("")
+    val actual = input.map(HelloWorld.extractCode)
+
+    Then("")
+    val expected = List(110, 11, 2, 2)
+
+    actual should contain theSameElementsAs expected
+  }
+
   test("Je veux ajouter une colonne avec la moyenne des numéros de département par région") {
     Given("Une dataframe avec 4 colonnes : code département, nom département, code région et nom région")
-    val inputDf = spark.sparkContext.parallelize(List(
+    val inputDf = List(
       ("01", "Ain", 84, "Auvergne-Rhône-Alpes"),
       ("02", "Aisne", 32, "Hauts-de-France"),
       ("03", "Allier", 84, "Auvergne-Rhône-Alpes"),
       ("04", "Alpes-de-Haute-Provence", 93, "Provence-Alpes-Côte d'Azur")
-    )).toDF("code_departement", "nom_departement", "code_region", "nom_region")
+    ).toDF("code_departement", "nom_departement", "code_region", "nom_region")
 
     When("")
     val actualDf = HelloWorld.avgDepByReg(inputDf)
 
     Then("")
-    val expectedDf = spark.sparkContext.parallelize(List(
+    val expectedDf = List(
       (32, "Hauts-de-France", 2.0),
       (84, "Auvergne-Rhône-Alpes", 2.0),
       (93, "Provence-Alpes-Côte d'Azur", 4.0)
-    )).toDF("code_region", "nom_region", "avg(code_departement)")
+    ).toDF("code_region", "nom_region", "avg(code_departement)")
 
     assertDataFrameEquals(actualDf, expectedDf)
   }
 
   test("Je veux renommer avg(code_departement) en avg_dep") {
     Given("Une dataframe avec 3 colonnes : code région, nom région et moyenne du numéro de département")
-    val inputDef = spark.sparkContext.parallelize(List(
+    val inputDef = List(
       (32, "Hauts-de-France", 2.0),
       (84, "Auvergne-Rhône-Alpes", 2.0),
       (93, "Provence-Alpes-Côte d'Azur", 4.0)
-    )).toDF("code_region", "nom_region", "avg(code_departement)")
+    ).toDF("code_region", "nom_region", "avg(code_departement)")
 
     When("")
-    val actualDf = HelloWorld.renameColumn(inputDef, "avg(code_departement)", "avg_dep")
+    val actualDf = HelloWorld.renameColumns(
+      inputDef,
+      Map("avg(code_departement)" -> "avg_dep")
+    )
 
-    val expectedDf = spark.sparkContext.parallelize(List(
+    val expectedDf = List(
       (32, "Hauts-de-France", 2.0),
       (84, "Auvergne-Rhône-Alpes", 2.0),
       (93, "Provence-Alpes-Côte d'Azur", 4.0)
-    )).toDF("code_region", "nom_region", "avg_dep")
+    ).toDF("code_region", "nom_region", "avg_dep")
 
     Then("")
     assertDataFrameEquals(actualDf, expectedDf)
@@ -89,17 +106,19 @@ class HelloWorldTest extends FunSuite with GivenWhenThen with DataFrameAssertion
     )).toDF("code_departement", "nom_departement", "code_region", "nom_region")
 
     When("")
-    val actualDf = HelloWorld.renameColumn(HelloWorld.avgDepByReg(inputDf), "avg(code_departement)", "avg_dep")
-    actualDf.write.format("parquet").mode("overwrite").save(output)
+    HelloWorld.renameColumns(
+      HelloWorld.avgDepByReg(inputDf),
+      Map("avg(code_departement)" -> "avg_dep")
+    ).write.mode(SaveMode.Overwrite).parquet(output)
 
     Then("I can read output file and find my values")
     val outputDf = spark.sqlContext.read.parquet(output)
 
-    val expectedDf = spark.sparkContext.parallelize(List(
+    val expectedDf = List(
       (32, "Hauts-de-France", 2.0),
       (84, "Auvergne-Rhône-Alpes", 2.0),
       (93, "Provence-Alpes-Côte d'Azur", 4.0)
-    )).toDF("code_region", "nom_region", "avg_dep")
+    ).toDF("code_region", "nom_region", "avg_dep")
 
     assertDataFrameEquals(outputDf, expectedDf)
   }
