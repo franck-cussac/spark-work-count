a933f93f9a8ffd8525f67b342e52ad57aada2ed0
Bristouflex <b.hounza@laposte.net>
2020-04-02 16:09:29 +0200

    nettoyage code + jointures

--- src/main/scala/xke/local/HelloWorld.scala
+++ src/main/scala/xke/local/HelloWorld.scala
@@ -1,22 +1,58 @@
 package xke.local
 
+import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.functions.udf
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
+
+import org.apache.spark.sql._
 
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
+    jointureDepartVille()
+
+  }
+
+  def jointureDepartVille(): Unit = {
     val spark = SparkSession.builder().master("local").appName("example of SparkSession").getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
     SparkSession.builder()
 
-    val df = spark.read.option("delimiter", ",").option("header", true).csv("src/main/resources/departements-france.csv")
+    val dfDepart = spark.read.option("delimiter", ",").option("header", true).csv("src/main/resources/departements-france.csv")
+    val dfVilles = spark.read.option("delimiter", ",").option("header", true).csv("src/main/resources/cities.csv")
+    val dfJoined = joinDepart(dfDepart, dfVilles)
+    dfJoined.show()
+
+  }
+
+  def joinDepart(dfDepart: DataFrame, dfVilles: DataFrame): DataFrame ={
+    dfDepart.join(dfVilles,
+      dfDepart("code_departement") === dfVilles("department_code"),
+      "left_outer"
+    )
+      .drop("department_code")
+  }
+
+  def departementExercises(): Unit ={
+    val spark = SparkSession.builder().master("local").appName("example of SparkSession").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    SparkSession.builder()
+
+    val df = spark.read.option("delimiter", ",").option("header", true).csv("src/main/resources/departements-france.csv").withColumn("code_departement", udfConvertToInt(col("code_departement")))
+    df.show()
     val newDF = avgDepByReg(dataFrame = df)
+    newDF.show()
     val newDfName = renameColumn(dataFrame = newDF, originalString = "avg(code_departement)", newName = "avg_dep")
     newDfName.show()
-    newDfName.write.parquet("src/main/resources/toto")
+    newDfName.write.mode("overwrite").parquet("src/main/resources/toto")
   }
+
+  def udfConvertToInt: UserDefinedFunction = udf(convertToInt)
+
+  def convertToInt: String => Int = (value: String) => value.filter(Character.isDigit).toInt
+
 
   def avgDepByReg(dataFrame: DataFrame): DataFrame = {
     dataFrame.groupBy("code_region", "nom_region").agg(avg("code_departement"))
--- src/test/scala/xke/local/HelloWorldTest.scala
+++ src/test/scala/xke/local/HelloWorldTest.scala
@@ -44,41 +44,23 @@ class HelloWorldTest extends FunSuite with GivenWhenThen with DataFrameAssertion
     assertDataFrameEquals(actual, expected)
   }
 
-/*  test("main must create a file with word count result") {
 
-    Given("input filepath and output filepath")
-    val input = "src/test/resources/input.txt"
-    val output = "src/test/resources/output/v1/parquet"
-
-    When("I call word count")
-    HelloWorld.main(Array(input, output))
-    val expected = spark.sparkContext.parallelize(
-      List(("rapidement",1),
-        ("te",1),
-        ("à",1),
-        ("mots",1),
-        ("des",1),
-        ("s'il",1),
-        ("compter",1),
-        ("Bonjour,",1),
-        ("as",1),
-        ("plait.",1),
-        ("tu",1))
-    ).toDF("word", "count")
-
-    Then("I can read output file and find my values")
-    val actually = spark.sqlContext.read.parquet(output)
-
-    assertDataFrameEquals(actually, expected)
-  }
-*/
   test("Je veux tester que la lecture d'un fichier, le calcul de moyenne de département selon la région, le renommage d'une colonne et l'écriture du résultat dans un parquet fonctionnent"){
     Given("le lancement de la méthode main")
-    HelloWorld.main(null)
+    HelloWorld.departementExercises()
     When("je vérifie les différentes colonnes du parquet")
     val df = spark.read.parquet("src/main/resources/toto")
     Then("les colonnes doivent être les même que dans le jeu de données de résultat")
     assert(df("avg_dep") != null && df("nom_region") != null && df("code_region") != null)
+  }
+
+  test("je veux tester que la conversion de string en int fonctionne"){
+    Given("une chaîne à convertir")
+    val input = "075"
+    When("Je convertis la chaîne d'entrée")
+    val expected = 75
+    Then("je m'attends à trouver 75")
+    assert(HelloWorld.convertToInt(input) === expected)
   }
 
 }
