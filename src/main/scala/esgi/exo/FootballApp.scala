ddc0180cabdd377ae7b8c2bacd61cbb289fef2eb
antoine cheval darracq <antoine.cheval.darracq@gmail.com>
2020-04-03 16:42:45 +0200

    exo2

--- src/main/scala/esgi/exo/FootballApp.scala
+++ src/main/scala/esgi/exo/FootballApp.scala
@@ -14,8 +14,11 @@ object FootballApp {
     val atHomeUDF = spark.udf.register("atHomeUDF",atHome)
 
     val dfexo1 = exo1(df)
-    dfexo1.withColumn("at_home", atHomeUDF(col("match"))).show()
+    val dfWithAtHome = dfexo1.withColumn("at_home", atHomeUDF(col("match")))
+    val dfexo2 = exo2(dfWithAtHome)
 
+    writeParquet(dfexo2)
+    dfexo2.show()
 
   }
 
@@ -42,9 +45,22 @@ object FootballApp {
 
   }
 
-  //def exo2(dataFrame: DataFrame): DataFrame = {
-  //dataFrame.withColumn("at_home", atHomeUDF(col("match"),"France"))
-  //}
+  def exo2(dataFrame: DataFrame): DataFrame = {
+    dataFrame.groupBy("adversaire").agg(
+      avg("score_france").alias("average_goals_france"),
+      avg("score_adversaire").alias("average_goals_opponent"),
+      count("match").alias("number_of_matches"),
+      count(when(col("at_home"), true)),
+      count(when(col("competition").startsWith("Coupe du monde"), col("competition"))).alias("number_of_WC_matches"),
+      sum(col("penalty_france")),
+      sum(col("penalty_adversaire"))
+    )
+      .withColumn("home_percentage", col("count(CASE WHEN at_home THEN true END)") / col("number_of_matches") * 100)
+      .withColumn("diff_penalty", col("sum(penalty_france)") - col("sum(penalty_adversaire)"))
+      .drop("sum(penalty_adversaire)")
+      .drop("sum(penalty_france)")
+      .drop("count(CASE WHEN at_home THEN true END)")
+  }
 
 
   def atHome = (detail: String) => {
@@ -56,16 +72,12 @@ object FootballApp {
     }
   }
 
-
-
-
-
-
+  
   def renameColumn(dataFrame: DataFrame, oldC : String, newC : String): DataFrame = {
     dataFrame.withColumnRenamed(oldC, newC)
   }
 
   def writeParquet(dataFrame: DataFrame): Unit = {
-    dataFrame.write.mode("overwrite").parquet("src/main/parquets/output.parquet")
+    dataFrame.write.mode("overwrite").parquet("src/main/parquets/stats.parquet")
   }
 }
