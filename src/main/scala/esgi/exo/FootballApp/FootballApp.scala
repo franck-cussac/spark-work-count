a6edd78839598fccf647f44d1a58deb6b26f0cd9
DEYEHE <jdeyehe@gmail.com>
2020-04-03 22:19:00 +0200

    project

--- src/main/scala/esgi/exo/FootballApp/FootballApp.scala
+++ src/main/scala/esgi/exo/FootballApp/FootballApp.scala
@@ -1,22 +1,94 @@
 package esgi.exo.FootballApp
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql._
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.{avg, _}
 
 object FootballApp {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("FootballApp").master("local[*]").getOrCreate()
-    val dfMatches = spark.read.option("header", true).csv("src/main/resources/df_matches.csv")
+    val isDomicile: UserDefinedFunction = udf(isHome _)
+    //val sumMatch: UserDefinedFunction = udf(nbMatch _)
 
-    val dfMatchesClean = cleanDF(dfMatches)
-    dfMatchesClean.show
+    val spark = SparkSession.builder().appName("FootballApp").master("local[*]").getOrCreate()
+    val df = spark.read.option("header", true).csv("src/main/resources/df_matches.csv")
+    df.show(500)
+
+    val dfMatchesClean = renameAndSelect(df)
+    val dfMatches = deleteNullValue(dfMatchesClean)
+    val dfMatchFrance = dfMatches.filter(col("date").between("1980-03-01",current_date())).withColumn("penalty_france",col("penalty_france").cast("integer")).withColumn("match_a_domicile",isDomicile(col("match")))
+    dfMatchFrance.show
+
+
+    val avgMatchFrance = avgScoreFrance(dfMatches)
+    avgMatchFrance
+      .show
+
+    val avgMatchAdv = avgScoreAdversaire(dfMatches)
+    avgMatchAdv.show
+
+    val nbMa = sumMatchParEquipe(dfMatches)
+    nbMa.show
+
+    /*dfMatchesFrance
+      .groupBy("adversaire")
+      .agg(sum(when(col("match_a_domicile") === true, 1)) / count(col("match_a_domicile")) * 100).show*/
+
+    val dfPercentageMatchHome= dfMatchFrance.groupBy("adversaire").agg(sum(when(col("match_a_domicile") === true,1)) / count(col("match_a_domicile")))
+
+    dfPercentageMatchHome.show
+
+    dfMatchFrance
+      .groupBy("match")
+      .max("penalty_france")
+      .show(100)
+
 
   }
 
-
-  def cleanDF(dataFrame: DataFrame) : DataFrame = {
+  def renameAndSelect(dataFrame: DataFrame) : DataFrame = {
     dataFrame
       .withColumnRenamed("X4","match")
       .withColumnRenamed("X6","competition")
+      .withColumn("date",col("date").cast("date"))
       .select("match","competition","adversaire","score_france","score_adversaire","penalty_france","penalty_adversaire","date")
   }
+
+  def deleteNullValue(dataFrame: DataFrame) : DataFrame = {
+    dataFrame
+      .withColumn("penalty_france",when(col("penalty_france")==="NA" || col("penalty_france").isNull ,"0"))
+      .withColumn("penalty_adversaire",when(col("penalty_adversaire") ==="NA" || col("penalty_adversaire").isNull,"0"))
+  }
+
+
+  def isHome(colName: String): Boolean = {
+    val eq = col(colName).toString().split('-')(0).trim
+    eq match {
+      case "France" => true
+      case _ => false
+    }
+  }
+
+  def avgScoreFrance(df: DataFrame): DataFrame = {
+    df.groupBy("adversaire")
+      .agg(avg(col("score_france").cast("integer")))
+  }
+
+  def avgScoreAdversaire (df: DataFrame): DataFrame = {
+    df.groupBy("adversaire")
+      .agg(avg(col("score_adversaire").cast("integer")))
+  }
+
+  def nbMatch(df:DataFrame): Integer = {
+    df.collect().length
+  }
+
+  def sumMatchParEquipe(df:DataFrame): DataFrame = {
+    df.groupBy("adversaire")
+      .count()
+      .as("Nombre de match")
+  }
+
+
+
 }
--- src/test/scala/esgi/exo/FootballAppTest/FootballAppTest.scala
+++ src/test/scala/esgi/exo/FootballAppTest/FootballAppTest.scala
@@ -1,9 +1,52 @@
 package esgi.exo.FootballAppTest
 
+import esgi.exo.FootballApp.FootballApp
 import org.scalatest.{FunSuite, GivenWhenThen}
 import spark.{DataFrameAssertions, SharedSparkSession}
 
 class FootballAppTest  extends FunSuite with GivenWhenThen with DataFrameAssertions {
   val spark = SharedSparkSession.sparkSession
   import spark.implicits._
+
+  test("je veux selectionner certaines colonnes") {
+    Given("Je crée ma dataframe")
+   val input = spark.sparkContext.parallelize(List(
+     ("1er mai 1904","Belgique - France", "3-3","Match amical","Belgique","3","3","NA","NA","1904-05-01","1904","draw","1"),
+     ("2 mars 2007","Allemagne - France", "0-3","Match amical","Allemagne","3","3","NA","NA","1904-05-01","1904",  "draw", "1"),
+     ("15 juin mai 2014","France-Algerie", "1-0","Coupe du monde","France","0","1","NA","NA","2014-06-15","2014",  "draw", "3")
+   )).toDF("X2", "X4","X5","X6","adversaire","score_france","score_adversaire","penalty_france","penalty_adversaire","date","year","outcome","no")
+
+    When("Je fait appel à ma fonction renameAndSelect")
+    val actual = FootballApp.renameAndSelect(input)
+    val expected = 8
+    Then("Je dois avoir 8 colonnes")
+    assert(actual.columns.length === expected)
+  }
+
+  test("je ne veux plus de valeur null") {
+    Given("Je crée ma dataframe")
+    val input = spark.sparkContext.parallelize(List(
+      ("1er mai 1904","Belgique - France", "3-3","Match amical","Belgique","3","3","NA","NA","1904-05-01","1904","draw","1"),
+      ("2 mars 2007","Allemagne - France", "0-3","Match amical","Allemagne","3","3","NA","NA","1904-05-01","1904",  "draw", "1"),
+      ("2 mars 2007","Allemagne - France", "0-3","Match amical","Allemagne","3","3","NA","NA","1904-05-01","1904",  "draw", "1")
+
+    )).toDF("X2", "X4","X5","X6","adversaire","score_france","score_adversaire","penalty_france","penalty_adversaire","date","year","outcome","no")
+
+    When("Je fait appel à ma fonction renameAndSelect")
+    val actual = FootballApp.deleteNullValue(input)
+
+    val expected = spark.sparkContext.parallelize(List(
+      ("1er mai 1904","Belgique - France", "3-3","Match amical","Belgique","3","3","0","0","1904-05-01","1904","draw","1"),
+      ("2 mars 2007","Allemagne - France", "0-3","Match amical","Allemagne","3","3","0","0","1904-05-01","1904",  "draw", "1"),
+      ("2 mars 2007","Allemagne - France", "0-3","Match amical","Allemagne","3","3","0","0","1904-05-01","1904",  "draw", "1")
+
+    )).toDF("X2", "X4","X5","X6","adversaire","score_france","score_adversaire","penalty_france","penalty_adversaire","date","year","outcome","no")
+
+    Then("Je dois avoir les colonnes match,competition,adversaire,score_france,score_adversaire,penalty_france,penalty_adversaire,date")
+    assertDataFrameEquals(actual,expected)
+
+  }
+
+
+
 }
