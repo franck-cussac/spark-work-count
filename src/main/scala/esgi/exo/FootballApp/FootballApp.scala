dc10bdc47cd17240b70b410acd2586135a9185c2
DEYEHE <jdeyehe@gmail.com>
2020-04-03 14:06:11 +0200

    clean df

--- src/main/scala/esgi/exo/FootballApp/FootballApp.scala
+++ src/main/scala/esgi/exo/FootballApp/FootballApp.scala
@@ -1,13 +1,22 @@
 package esgi.exo.FootballApp
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object FootballApp {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("FootballApp").master("local[*]").getOrCreate()
     val dfMatches = spark.read.option("header", true).csv("src/main/resources/df_matches.csv")
 
-    dfMatches.show
+    val dfMatchesClean = cleanDF(dfMatches)
+    dfMatchesClean.show
 
+  }
+
+
+  def cleanDF(dataFrame: DataFrame) : DataFrame = {
+    dataFrame
+      .withColumnRenamed("X4","match")
+      .withColumnRenamed("X6","competition")
+      .select("match","competition","adversaire","score_france","score_adversaire","penalty_france","penalty_adversaire","date")
   }
 }
